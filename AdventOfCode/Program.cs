5e232a928cf7da5783e9d081e9721f1df99b1bf2
Roland Kock <roland.kock@visma.com>
2022-12-02 11:15:09 +0100

    reverted default program.cs

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,5 +1,18 @@
-await Solver.SolveAll(opt =>
+if (args.Length == 0)
 {
-    opt.ShowConstructorElapsedTime = true;
-    opt.ShowTotalElapsedTimePerDay = true;
-});
+    await Solver.SolveLast(opt => opt.ClearConsole = false);
+}
+else if (args.Length == 1 && args[0].Contains("all", StringComparison.CurrentCultureIgnoreCase))
+{
+    await Solver.SolveAll(opt =>
+    {
+        opt.ShowConstructorElapsedTime = true;
+        opt.ShowTotalElapsedTimePerDay = true;
+    });
+}
+else
+{
+    var indexes = args.Select(arg => uint.TryParse(arg, out var index) ? index : uint.MaxValue);
+
+    await Solver.Solve(indexes.Where(i => i < uint.MaxValue));
+}
